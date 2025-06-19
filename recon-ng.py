17f23aab50c0721c08584814fab93b102fbac65a
m4tt <jjschroder63@gmail.com>
2025-06-19 15:04:40 +0200

    Update recon-ng.py

--- recon-ng.py
+++ recon-ng.py
@@ -1,17 +1,26 @@
-#!/usr/bin/python -tt
+#!/usr/bin/env python3
 
-print """
-    _/_/_/    _/_/_/_/    _/_/_/    _/_/    _/      _/              _/      _/    _/_/_/
-   _/    _/  _/        _/        _/    _/  _/_/    _/              _/_/    _/  _/
-  _/_/_/    _/_/_/    _/        _/    _/  _/  _/  _/  _/_/_/_/_/  _/  _/  _/  _/  _/_/
- _/    _/  _/        _/        _/    _/  _/    _/_/              _/    _/_/  _/    _/
-_/    _/  _/_/_/_/    _/_/_/    _/_/    _/      _/              _/      _/    _/_/_/
-
-This script has been replaced by the Recon-ng Framework.
-
-https://bitbucket.org/LaNMaSteR53/recon-ng
-
-Thank you.
-
-@LaNMaSteR53  
 """
+GXFR replicates DNS zone transfers by enumerating subdomains using advanced search engine queries
+and conducting DNS lookups.
+Original Author: Tim Tomes (LaNMaSteR53)
+Ported to Python 3 and improved for modern compatibility.
+m4tth4ck
+Note on shebang:
+  The original line '#!/usr/bin/python -tt' was used in Python 2 to enforce strict tab/space usage.
+  In Python 3, the '-tt' option is deprecated and has no effect. This version uses the modern
+  shebang: '#!/usr/bin/env python3'
+"""
+print("                                                                                            ")
+print("        _/_/_/    _/_/_/_/    _/_/_/    _/_/    _/      _/              _/      _/    _/_/_/")
+print("       _/    _/  _/        _/        _/    _/  _/_/    _/              _/_/    _/  _/       ") 
+print("      _/_/_/    _/_/_/    _/        _/    _/  _/  _/  _/  _/_/_/_/_/  _/  _/  _/  _/  _/_/  ")
+print("     _/    _/  _/        _/        _/    _/  _/    _/_/              _/    _/_/  _/    _/   ")
+print("    _/    _/  _/_/_/_/    _/_/_/    _/_/    _/      _/              _/      _/    _/_/_/    ")
+print("                                                                                            ")
+print("    This script has been replaced by the Recon-ng Framework.                                ")
+print("                                                                                            ")
+print("    https://bitbucket.org/LaNMaSteR53/recon-ng                                              ")
+print("                                                                                            ")
+print("    Thank you.@LaNMaSteR53                                                                  ")
+print("                                                                                            ")
