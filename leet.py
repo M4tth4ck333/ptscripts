766bf9294e51befd1daf620246a86092f21fae6d
m4tt <jjschroder63@gmail.com>
2025-06-19 17:14:27 +0200

    Update leet.py
    updated and added enum features

--- leet.py
+++ leet.py
@@ -1,39 +1,35 @@
-#!/usr/bin/python -tt
+#!/usr/bin/env python3
 
 import sys
 
-leetdict = { \
-'a':['4','@'], \
-'e':['3'], \
-'g':['6'], \
-'i':['1','!'], \
-'l':['7','1','!'], \
-'n':['^'], \
-'o':['0'], \
-'q':['0'], \
-'s':['5','$'], \
-'t':['7'], \
-'v':['\/'], \
+leetdict = {
+    'a': ['4', '@'],
+    'e': ['3'],
+    'g': ['6'],
+    'i': ['1', '!'],
+    'l': ['7', '1', '!'],
+    'n': ['^'],
+    'o': ['0'],
+    'q': ['0'],
+    's': ['5', '$'],
+    't': ['7'],
+    'v': ['\\/'],
 }
 
-# for JTR
-# X = position
-# Y = letter
-# Z = leet substitiute
-#rule = '=XYoXZ'
 def jtr(num):
-  print '[List.Rules:Wordlist]'
-  for key in leetdict.keys():
-    for val in leetdict[key]:
-      for i in range(int(num)):
-        print '=%s%so%s%s' % (i, key, i, val)
-  
+    print('[List.Rules:Wordlist]')
+    for key in leetdict.keys():
+        for val in leetdict[key]:
+            for i in range(int(num)):
+                print(f'={i}{key}o{i}{val}')
+
 def usage():
-  print """
+    print("""
 leet.py - Tim Tomes (@LaNMaSteR53) (www.lanmaster53.com)
 
 Usage:
   ./leet.py [options]
+
 Options:
   -h          - This screen
   -c          - Swap case of all letters
@@ -42,54 +38,71 @@ Options:
   -b <#chars> - Build a John The Ripper leet mangle rule for words <#chars> long.
                 - Uses custom mangle dictionary as seen with '-v'
                 - Not as thorough.
-  """
+    """)
 
 def case(wordlist):
-  for word in wordlist:
-    for i in range(len(word)):
-      chars = list(word)
-      chars[i] = chars[i].swapcase()
-      neword = ''.join(chars)
-      if not neword in wordlist:
-        wordlist.append(neword)
-  return wordlist
+    result = list(wordlist)  # preserve original
+    for word in wordlist:
+        for i in range(len(word)):
+            chars = list(word)
+            chars[i] = chars[i].swapcase()
+            neword = ''.join(chars)
+            if neword not in result:
+                result.append(neword)
+    return result
 
 def leet(wordlist):
-  for word in wordlist:
-    for i in range(len(word)):
-      chars = list(word)
-      if chars[i].lower() in leetdict.keys():
-        for x in leetdict[chars[i].lower()]:
-          chars[i] = x
-          neword = ''.join(chars)
-          if not neword in wordlist:
-            wordlist.append(neword)
-  return wordlist
+    result = list(wordlist)
+    for word in wordlist:
+        for i in range(len(word)):
+            chars = list(word)
+            if chars[i].lower() in leetdict:
+                for x in leetdict[chars[i].lower()]:
+                    chars[i] = x
+                    neword = ''.join(chars)
+                    if neword not in result:
+                        result.append(neword)
+    return result
 
-wordlist = []
+def main():
+    wordlist = []
 
-if len(sys.argv) == 3 and sys.argv[1] == '-b':
-  jtr(sys.argv[2])
-  sys.exit()
-if len(sys.argv) == 2 and sys.argv[1] == '-v':
-  for key in sorted(leetdict.keys()):
-    print '%s:%s' % (key,','.join(leetdict[key]))
-  sys.exit()
-if '-h' in sys.argv:
-  usage()
-  sys.exit()
-if '-f' in sys.argv and len(sys.argv) >= 3:
-  filename = sys.argv[sys.argv.index('-f') + 1]
-  if filename == '-':
-    wordlist = sys.stdin.read().split()
-  else:
-    wordlist = open(filename).read().split()
-if '-c' in sys.argv:
-  wordlist = case(wordlist)
-if wordlist == []:
-  usage()
-  sys.exit()
+    if len(sys.argv) == 3 and sys.argv[1] == '-b':
+        jtr(sys.argv[2])
+        sys.exit()
 
-wordlist = leet(wordlist)
-for word in sorted(wordlist):
-  print word
+    if len(sys.argv) == 2 and sys.argv[1] == '-v':
+        for key in sorted(leetdict.keys()):
+            print(f'{key}:{",".join(leetdict[key])}')
+        sys.exit()
+
+    if '-h' in sys.argv:
+        usage()
+        sys.exit()
+
+    if '-f' in sys.argv:
+        try:
+            filename = sys.argv[sys.argv.index('-f') + 1]
+        except IndexError:
+            usage()
+            sys.exit()
+
+        if filename == '-':
+            wordlist = sys.stdin.read().split()
+        else:
+            with open(filename, 'r', encoding='utf-8') as f:
+                wordlist = f.read().split()
+
+    if '-c' in sys.argv:
+        wordlist = case(wordlist)
+
+    if not wordlist:
+        usage()
+        sys.exit()
+
+    wordlist = leet(wordlist)
+    for word in sorted(wordlist):
+        print(word)
+
+if __name__ == '__main__':
+    main()
